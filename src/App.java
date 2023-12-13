cda29b6552692059cb1af3cdbd799ede78af81b5
Paul841 <153355150+Paul841@users.noreply.github.com>
2023-12-13 19:44:01 +0100

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,23 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Creem les llistes
+        LlistaUsuaris llistaUsuaris = new LlistaUsuaris(10);
+        LlistaActivitat llistaActivitat = new LlistaActivitat(10);
+        LlistaReserva llistaReserva = new LlistaReserva(10);
+        LlistaEntitat llistaEntitat = new LlistaEntitat(10);
+
+        //Llegim la informacio dels arxius
+        Arxius.llegirArxiuUsuari(llistaUsuaris);
+        Arxius.llegirArxiuActivitat(llistaActivitat);
+        Arxius.llegirArxiuReserva(llistaReserva);
+        Arxius.llegirArxiuEntitat(llistaEntitat);
+
+        //Per a comprovar per pantalla que s'hagin llegit be
+       System.out.println(llistaActivitat.getActivitatPosicio(0));
+       System.out.println(llistaUsuaris.getUsuariPosicio(0));
+       System.out.println(llistaReserva.getReservaPosicio(0));
+       System.out.println(llistaEntitat.getEntitatsPosicio(0));
+
+        
     }
 }
